474198bb7d0e3f8a616b29bc9c63281ba4cc53f1
lvlin <boalai@qq.com>
2015-11-02 17:37:27 +0800

    add more items

---
+++ item3/js/auto_combine.js
@@ -0,0 +1,328 @@
+loader.show(function() {
+	$("#Jmain").show(), Index.stageFun()
+}, ["http://res.bjx.netease.com/gw/15v2/m/img/book01_7c33092.png", "http://res.bjx.netease.com/gw/15v2/m/img/book02_fe091d9.png", "http://res.bjx.netease.com/gw/15v2/m/img/icon01_3661793.png", "http://res.bjx.netease.com/gw/15v2/m/img/icon02_b81f65d.png", "http://res.bjx.netease.com/gw/15v2/m/img/news01_09d806e.png", "http://res.bjx.netease.com/gw/15v2/m/img/news02_626ce1c.png", "http://res.bjx.netease.com/gw/15v2/m/img/news03_14b139d.png", "http://res.bjx.netease.com/gw/15v2/m/img/news04_04726af.png", "http://res.bjx.netease.com/gw/15v2/m/img/news05_531c537.png", "http://res.bjx.netease.com/gw/15v2/m/img/news06_1785cae.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage01_853e193.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage01_2_a4a1306.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage01_3_68be4a5.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage01_4_fb908bd.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage01_5_dfd240b.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage01_6_a331fa9.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage01_7_394ee85.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage01_8_f25ec10.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage02_7af93a7.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage02_2_f9148d3.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage02_3_8287b8a.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage02_4_032e31c.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage02_5_81870e1.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage02_6_b6e92f5.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage02_7_266240f.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage03_50c1016.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage03_2_fea52f7.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage03_3_4c94651.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage03_4_c1cfad5.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage03_5_b3d5caf.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage03_6_a2bec28.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage03_7_7ed8ce8.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage03_8_4960956.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage04_cc148f0.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage04_2_28db5ab.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage04_3_418b71f.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage04_4_3149ebc.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage04_5_21fd663.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage04_6_a7a3c12.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage04_7_6277310.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage05_6ae4d92.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage05_2_e8b4d97.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage05_3_098188e.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage05_4_d600db7.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage05_5_0483377.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage05_6_f798780.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage05_7_5f963da.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage05_8_a4a1c24.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage05_9_09ad7c7.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage06_9866a67.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage06_2_10032d1.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage06_3_de8a1aa.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage06_4_7a6faa3.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage06_4_1_64cf447.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage06_4_2_211cfc4.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage06_4_3_fa14999.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage06_4_4_30e0569.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage06_4_5_6e6af6c.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage06_4_6_66f8b1d.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage06_5_1_fc0867e.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage06_6_1_bfb7ceb.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage06_7_1_9984095.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage06_8_1_dbaf443.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage06_9_1_de3a5a8.png", "http://res.bjx.netease.com/gw/15v2/m/img/stage06_10_1_342119f.png"]);
+var Index = function() {
+	var t, e, i, n, s = 0,
+		o = navigator.userAgent.toLowerCase(),
+		a = /micromessenger/i.test(o),
+		c = /yixin/i.test(o),
+		g = !1,
+		r = $("#Jheader"),
+		m = $("#Jmain"),
+		p = ($("#specile"), $("#Jbtntop")),
+		d = ($("#finger_printer"), $("#share"), $("#btn_close_sign"), $("#Jnewsbtn")),
+		l = $("#Jart"),
+		w = $("#Jnews"),
+		h = $("#Jnewslist"),
+		b = $("#Jlaststage .swiper-wrapper"),
+		u = function() {
+			$("html").css("font-size", 62.5 * $(window).width() / 320 + "%"), m.css("min-height", $(window).height()), $(window).bind("touchmove", function(t) {
+				t.preventDefault()
+			}), $("#specile .arrow").bind("click", function() {
+				t.slideNext()
+			}), _(), x(), v(), z(), J(), C()
+		}, _ = function() {
+			var t = $("#Jvideo"),
+				e = t.find("video");
+			$("#specile .stage-video").bind("click", function() {
+				var i = $(this).attr("data-mp4");
+				e.attr("src", i), t.show(), e.get(0).play()
+			}), t.find(".bg").bind("click", function() {
+				e.get(0).pause(), t.hide(), e.attr("src", "")
+			})
+		}, f = function() {
+			var e = $("#specile .stage-white");
+			t = new Swiper("#specile", {
+				direction: "vertical",
+				speed: 400,
+				followFinger: !1,
+				onReachEnd: function() {
+					i.onResize(), g = !0, r.hide(), b.css({
+						transform: "translate3d(0px, -1px, 0px)"
+					})
+				},
+				onTransitionEnd: function(t) {
+					e.show(), setTimeout(function() {
+						e.eq(t.activeIndex - 1).hide()
+					}, 2200)
+				}
+			}), u(), k()
+		}, x = function() {
+			var e = $("#about_swiper_txt .slide-txt");
+			about_swiper = new Swiper("#about_swiper", {
+				paginationClickable: !0,
+				direction: "horizontal",
+				speed: 300,
+				loop: !0,
+				initialSlide: 1,
+				centeredSlides: !0,
+				autoplay: 4e3,
+				spaceBetween: 20,
+				slidesPerView: 3,
+				onTransitionEnd: function() {
+					var t = $("#about_swiper .swiper-slide-active").attr("data-swiper-slide-index");
+					e.removeClass("show").hide(), e.eq(t).show(), setTimeout(function() {
+						e.eq(t).addClass("show")
+					}, 10)
+				}
+			}), i = new Swiper("#Jlaststage", {
+				scrollbar: "#Jlaststage .swiper-scrollbar",
+				direction: "vertical",
+				slidesPerView: "auto",
+				freeMode: !0,
+				onReachBeginning: function() {
+					g && "translate3d(0px, 0px, 0px)" == b.css("transform") && (t.slidePrev(), g = !1, r.show())
+				}
+			}), p.bind("click", function() {
+				t.slideTo(0), g = !1, r.show()
+			})
+		}, v = function() {
+			var t = $("#Jslidefinger span");
+			t.bind("touchstart", function() {
+				if (!$(this).hasClass("press")) {
+					var t = this,
+						e = $(t).attr("data-href");
+					s = (new Date).getTime(); {
+						setTimeout(function() {
+							$(t).addClass("press"), n = setTimeout(function() {
+								j(e)
+							}, 1e3)
+						}, 0)
+					}
+				}
+			}), t.bind("touchend", function() {
+				var t = $(this);
+				if (t.hasClass("press")) {
+					var e = (new Date).getTime(),
+						i = t.attr("data-href");
+					1e3 > e - s ? (t.removeClass("press"), clearTimeout(n), s = 0) : j(i)
+				}
+			})
+		}, j = function(t) {
+			window.location.href = t
+		}, C = function() {
+			e = new Swiper("#news_scroll", {
+				scrollbar: "#news_scroll .swiper-scrollbar",
+				direction: "vertical",
+				slidesPerView: "auto",
+				freeMode: !0
+			});
+			new Swiper("#nav_swiper", {
+				scrollbar: "#nav_swiper .swiper-scrollbar",
+				direction: "horizontal",
+				slidesPerView: "auto",
+				freeMode: !0
+			});
+			$("#news_swiperA .swiper-slide,#news_swiperB .swiper-slide").each(function() {
+				var t = $(this);
+				0 == t.find("a").length && t.remove()
+			});
+			var t = new Swiper("#news_swiperA", {
+				pagination: "#news_swiperA .swiper-pagination",
+				paginationClickable: !0,
+				direction: "horizontal",
+				speed: 500,
+				loop: !1,
+				onInit: function() {}
+			}),
+				i = new Swiper("#news_swiperB", {
+					pagination: "#news_swiperB .swiper-pagination",
+					paginationClickable: !0,
+					direction: "horizontal",
+					speed: 500,
+					loop: !1
+				}),
+				n = new Swiper("#news_swiperC", {
+					pagination: "#news_swiperC .swiper-pagination",
+					paginationClickable: !0,
+					direction: "horizontal",
+					speed: 500,
+					loop: !1,
+					preloadImages: !1,
+					lazyLoading: !0
+				});
+			$("#Jart .back-btn").bind("click", function() {
+				T()
+			}), d.bind("click", function() {
+				w.addClass("show"), e.onResize()
+			}), $("#Jnewsbackbtn").bind("click", function() {
+				w.removeClass("show")
+			});
+			var s = $("#Jnewslist .news-swiper"),
+				o = $("#nav_swiper .nav_swiper_a"),
+				a = [t, i, n];
+			o.bind("click", function() {
+				var t = $(this),
+					e = t.attr("data-index");
+				o.removeClass("cur"), t.addClass("cur"), s.hide().removeClass("show"), s.eq(e).show(), setTimeout(function() {
+					s.eq(e).addClass("show"), a[e].update()
+				}, 10)
+			})
+		}, k = function() {
+			ADBase.config({
+				pos: "baijiangxing-lunhuan-652",
+				callback: function(t, e) {
+					if (0 == t) return !1;
+					var i = e["baijiangxing-lunhuan-652"],
+						n = "";
+					n += '<div class="swiper-wrapper">';
+					for (var s = 0; s < i.length; s++) {
+						var o = i[s];
+						n += '<div class="swiper-slide"><a href="javascript:;" action="link" data-link="' + o.url + '" title="' + o.title + '"><img src="' + o.src + '" /><div class="swiper-bg"></div><div class="swiper-bgA"></div></a></div>'
+					}
+					n += '</div><div class="swiper-pagination"></div>', $("#banner_swiper").html(n);
+					new Swiper("#banner_swiper", {
+						pagination: "#banner_swiper .swiper-pagination",
+						paginationClickable: !0,
+						direction: "horizontal",
+						speed: 500,
+						loop: !1
+					});
+					$('#Jnewslist a[action="link"]').bind("click", function() {
+						var t = $(this).attr("data-link");
+						t && "" != t && (y(), I(t))
+					})
+				}
+			})
+		}, y = function() {
+			h.removeClass("show"), l.show(), setTimeout(function() {
+				h.hide(), l.addClass("show"), e.onResize()
+			}, 280)
+		}, T = function() {
+			l.removeClass("show"), setTimeout(function() {
+				l.hide(), h.show().addClass("show"), e.onResize()
+			}, 280)
+		}, I = function(t) {
+			$.ajax({
+				url: t,
+				dataType: "html",
+				type: "get",
+				success: function(i) {
+					var n = $(i);
+					return n.find("#NIE-art").get(0) ? ($("#Jart .art-tit h1").html(n.find("#NIE-art .artTitle").html()), $("#Jart .art-time").html("发布：" + n.find("#NIE-art .artDate").html()), $("#Jart .art-con").html(n.find("#NIE-art .artText").html()), void setTimeout(function() {
+						e.onResize(), e.slideTo(0)
+					}, 300)) : void j(t)
+				},
+				error: function() {
+					j(t)
+				}
+			})
+		}, J = function() {
+			var t = $("#forhorview");
+			window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", function() {
+				90 == window.orientation || -90 == window.orientation ? t.show() : t.hide()
+			}, !1)
+		}, z = function() {
+			if (document.getElementById("share_title")) {
+				var t = {
+					title: document.getElementById("share_title").innerHTML,
+					desc: document.getElementById("share_desc").innerHTML,
+					url: window.location.href,
+					imgurl: document.getElementById("share_pic").getAttribute("data-src")
+				};
+				nie.use(["nie.util.mobiShare"], function() {
+					$(function() {
+						MobileShare.setting({
+							title: t.title,
+							desc: t.desc,
+							url: t.url,
+							imgurl: t.imgurl,
+							callback: function() {}
+						})
+					})
+				}), $("#sina_share").click(function() {
+					var e = "http://service.weibo.com/share/share.php?c=nie&content=gb2312&source=nie&title=" + encodeURIComponent(t.desc) + "&url=" + encodeURIComponent(t.url) + "&pic=" + encodeURIComponent(t.imgurl);
+					window.location.href = e
+				}), $("#qzone_share").click(function() {
+					var e = "http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?summary=" + encodeURIComponent("网易游戏") + "&url=" + encodeURIComponent(t.url) + "&title=" + encodeURIComponent(t.title) + "&desc=" + encodeURIComponent(t.desc) + "&pics=" + encodeURIComponent(t.imgurl);
+					window.location.href = e
+				}), $("#yx_share").click(function() {
+					var e = "http://open.yixin.im/share?appkey=yx3ae08a776bf04178a583cb745fb6aa0c&type=webpage&url=" + encodeURIComponent(t.url) + "&title=" + encodeURIComponent(t.title) + "&desc=" + encodeURIComponent(t.desc) + "&pic=" + encodeURIComponent(t.imgurl);
+					window.location.href = e
+				});
+				var e = $("#share"),
+					i = $("#share-weixin"),
+					n = $("#moudle");
+				$("#btn-share").click(function() {
+					a || c ? i.addClass("show") : e.addClass("show").show()
+				}), i.click(function() {
+					i.removeClass("show")
+				}), e.click(function() {
+					e.removeClass("show").hide()
+				}), $("#btn-public").click(function() {
+					n.addClass("show").show()
+				}), n.click(function() {
+					n.removeClass("show").hide()
+				})
+			}
+		};
+	return {
+		stageFun: f
+	}
+}(),
+	bookFun = function() {
+		var t = !1,
+			e = $("#btn_order"),
+			i = $("#Jbook"),
+			n = $("#Jbook .close_btn"),
+			s = $("#phone"),
+			o = window.location.href,
+			a = function() {
+				$(".book-btn").bind("click", function() {
+					i.show();
+					setTimeout(function() {
+						i.addClass("show")
+					}, 50)
+				}), n.bind("click", function() {
+					i.removeClass("show");
+					setTimeout(function() {
+						i.hide()
+					}, 300)
+				}), e.bind("click", function() {
+					r()
+				}), s.focus(function() {
+					n.hide()
+				}).blur(function() {
+					n.show()
+				})
+			}, c = function(t) {
+				return t.match(/^(13|14|15|18|17)\d{9}$/)
+			}, g = function() {
+				var t = navigator.userAgent.toLowerCase();
+				return /ios|iphone|ipad/i.test(t) ? "ios" : /android/i.test(t) ? "android" : void 0
+			}, r = function() {
+				if (m("提交中..."), !t) {
+					t = !0;
+					var e = s.val();
+					if (!c(e)) return m("请填写正确的手机号码"), void(t = !1);
+					var n = g();
+					$.ajax({
+						url: "http://mobile-game-appoint.webapp.163.com/appoint/ma42-" + n + "/" + e + "/" + n + "?src=" + o,
+						async: !1,
+						dataType: "jsonp",
+						success: function(e) {
+							t = !1, "ok" == e.status ? (m("恭喜你预约成功！"), s.val(""), i.removeClass("show"), setTimeout(function() {
+								i.hide()
+							}, 400)) : m(e.status)
+						},
+						error: function() {
+							t = !1, m("网络信号不好，请稍后刷新重试~")
+						}
+					})
+				}
+			}, m = function(t) {
+				var e = document.getElementById("Jtips");
+				e || (e = document.createElement("div"), e.id = "Jtips", e.className = "m-tips", e.style.cssText = "background:rgba(0,0,0,0.8); color: #fff;position: fixed; left: 50%; top: 40%; font-size: 1.4rem;width:18rem; padding: 3rem; line-height: 2.4rem; margin-top: -4rem;opacity: 0; display: none;z-index: 9999;text-align:center;border-radius:1rem;", document.body.appendChild(e)), e.innerHTML = t, e.style.display = "block", arguments[1] && (e.style.width = arguments[1]);
+				var i = e.clientWidth / 2;
+				e.style.marginLeft = "-" + i + "px", e.style.opacity = 1, setTimeout(function() {
+					e.style.webkitTransition = "all 0.3s ease-in", e.style.opacity = 0, e.style.marginTop = "-8rem", setTimeout(function() {
+						e.style.display = "none", e.style.marginTop = "-4rem"
+					}, 300)
+				}, 2e3)
+			};
+		return {
+			init: a,
+			showTips: m
+		}
+	}();
+bookFun.init();
